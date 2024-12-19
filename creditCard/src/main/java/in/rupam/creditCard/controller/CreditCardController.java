7c4dc63c086ad770861b1ed6151de3f4fcdf8fb8
rupamswain1 <rupamswain1@gmail.com>
2024-12-19 14:22:58 +0530

    methods added

--- creditCard/src/main/java/in/rupam/creditCard/controller/CreditCardController.java
+++ creditCard/src/main/java/in/rupam/creditCard/controller/CreditCardController.java
@@ -1,7 +1,44 @@
 package in.rupam.creditCard.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
+@RequestMapping("/api")
 public class CreditCardController {
+
+    @PostMapping("/addCard")
+    public void addCard(){
+
+    }
+
+    @GetMapping("/getCustomerCards")
+    public void getCardForCustomer(@RequestParam String customerMobileNumber){
+
+    }
+
+    @GetMapping("/getCard")
+    public void getCard(@RequestParam Long cardNumber){
+
+    }
+
+    @PutMapping("/updateDue")
+    public void updateDueAmount(){
+
+    }
+
+    @PutMapping("/updateCardDetails")
+    public void updateCardDetails(){
+
+    }
+
+    @DeleteMapping("/deleteCard")
+    public void deleteCard(){
+
+    }
+    @DeleteMapping("/deleteCustomer")
+    public void deleteCustomer(){
+
+    }
+
 }
