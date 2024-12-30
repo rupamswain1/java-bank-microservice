012ae7d566b6db016818b2a99bb630838b82ff86
rupamswain1 <rupamswain1@gmail.com>
2024-12-30 23:35:50 +0530

    get credit card and get customer credit cards added

--- creditCard/src/main/java/in/rupam/creditCard/service/ICreditCardService.java
+++ creditCard/src/main/java/in/rupam/creditCard/service/ICreditCardService.java
@@ -1,6 +1,9 @@
 package in.rupam.creditCard.service;
 
 import in.rupam.creditCard.dto.CreateCreditCardDto;
+import in.rupam.creditCard.dto.CreditCardResponseDto;
+
+import java.util.List;
 
 public interface ICreditCardService {
 
@@ -9,4 +12,18 @@ public interface ICreditCardService {
      * @param creditCardDto credit card details
      */
     public void createCreditCard(CreateCreditCardDto creditCardDto);
+
+    /**
+     *
+     * @param cardNumber credit card number
+     * @return credit card details
+     */
+    public CreditCardResponseDto gerCard(Long cardNumber);
+
+    /**
+     *
+     * @param customerMobileNumber
+     * @return List of Credit cards
+     */
+    public List<CreditCardResponseDto> getCustomerCards(String customerMobileNumber);
 }
