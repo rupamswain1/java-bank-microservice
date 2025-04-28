2991e10a40dff82ce6a9c4409b2f3ac73cf0f492
rupamswain1 <rupamswain1@gmail.com>
2025-04-28 23:46:30 +0530

    http timout and retry logic of accounts and loans

--- gatewayserver/src/main/java/in/rupam/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/in/rupam/gatewayserver/GatewayserverApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+
+import java.time.Duration;
 
 @SpringBootApplication
 public class GatewayserverApplication {
@@ -23,7 +26,11 @@ public class GatewayserverApplication {
 								))
 						.uri("lb://Accounts"))
 				.route(p->p.path("/javams/loans/**")
-						.filters(f->f.rewritePath("/javams/loans/(?<segment>.*)","/${segment}"))
+						.filters(f->f.rewritePath("/javams/loans/(?<segment>.*)","/${segment}")
+								.retry(retryConfig -> retryConfig.setRetries(3)
+										.setMethods(HttpMethod.GET)
+										.setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000),2,true))
+						)
 						.uri("lb://LOANS")
 				)
 				.route(p->p.path("/javams/creditcards/**")
